c593c800e60711231328ea550b1311351f72a450
Benjamin Daniel Huamani Bailon <benjalive12@gmail.com>
2020-11-10 12:58:23 -0500

    Update clase_creada.java

--- src/hola/clase_creada.java
+++ src/hola/clase_creada.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package hola;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
 public class clase_creada {
     public static void main(String[] args) {
         System.out.println("m"
