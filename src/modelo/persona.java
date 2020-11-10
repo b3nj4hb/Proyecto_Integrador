1e8032a91310418d05be75a616f00f1e96ddce97
J05K4 <73666329+josuedavidiazmoran@users.noreply.github.com>
2020-11-10 12:23:30 -0500

    Update persona.java
    ya hice tu trabajo putito

--- src/modelo/persona.java
+++ src/modelo/persona.java
@@ -1,6 +1,50 @@
 package modelo;
 public class persona {
-    int idpersona;
-    
+    private int idpersona;
+    private String nombre;
+    private String apellido;
+    private String perro;
+
+    public persona(int idpersona, String nombre, String apellido, String perro) {
+        this.idpersona = idpersona;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.perro = perro;
+    }
+
+    public persona() {
+    }
+
+    public int getIdpersona() {
+        return idpersona;
+    }
+
+    public void setIdpersona(int idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getPerro() {
+        return perro;
+    }
+
+    public void setPerro(String perro) {
+        this.perro = perro;
+    }
     
 }
