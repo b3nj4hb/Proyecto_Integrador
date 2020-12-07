5435028e6dca4f4631df6b5c004b0e188076f513
Benjamin Daniel Huamani Bailon <benjalive12@gmail.com>
2020-12-07 11:21:40 -0500

    Index
    cambio en el index, agregué los menus que levano pidió

--- src/utils/Leer.java
+++ src/utils/Leer.java
@@ -3,20 +3,22 @@ package utils;
 import java.util.Scanner;
 
 public class Leer {
-    public static Scanner teclado = new Scanner(System.in);
-    
-    public static String cadena(){
-        //nextLine() -->Leer Cadena
-        return teclado.nextLine();
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static String cadena() {
+        return sc.nextLine();
     }
-    public static int entero(){
+
+    public static int entero() {
         return Integer.valueOf(cadena());
     }
-    
-    public static double decimal(){
+
+    public static double decimal() {
         return Double.valueOf(cadena());
     }
-    public static char letra(){
+
+    public static char letra() {
         return cadena().charAt(0);
     }
 }
--- src/vistacontrol/Index.java
+++ src/vistacontrol/Index.java
@@ -1,6 +1,185 @@
-
 package vistacontrol;
 
+import utils.Leer;
+
 public class Index {
+
+    public static void menu_principal() {
+        System.out.println("1. Datos Autor");
+        System.out.println("2. Datos País");
+        System.out.println("3. Datos Editorial");
+        System.out.println("4. Datos Usuario");
+        System.out.println("5. Datos Libro");
+        System.out.println("6. Prestamos");
+        System.out.println("7. Devoluciones");
+        System.out.println("8. Salir");
+        System.out.print("Opcion [1-8]: ");
+    }
+    
+    public static void menu_secundario() {
+        System.out.println("1. Agregar");
+        System.out.println("2. Eliminar");
+        System.out.println("3. Editar");
+        System.out.println("4. Listar");
+        System.out.println("5. Salir");
+        System.out.print("Opcion [1-5]: ");
+    }
+    
+    public static void switches() {
+    int opcion;
+        do {
+            menu_principal();
+            opcion = Leer.entero();
+            switch (opcion) {
+                case 1:
+                    switch_autor();
+                    break;
+                case 2:
+                    switch_pais();
+                    break;
+                case 3:
+                    switch_editorial();
+                    break;
+                case 4:
+                    switch_usuario();
+                    break;
+                case 5:
+                    switch_libro();
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    System.out.println("Hasta la proxima...");
+                    break;
+                default:
+                switches();
+            }
+        } while (opcion != 8);
+    }
+    
+    public static void switch_autor() {
+        int opcion;
+        do {
+            menu_secundario();
+            opcion = Leer.entero();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    switches();
+                    break;
+                default:
+                switch_autor();
+            }
+        } while (opcion != 5);
+    }
+    
+    public static void switch_pais() {
+        int opcion;
+        do {
+            menu_secundario();
+            opcion = Leer.entero();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    switches();
+                    break;
+                default:
+                switch_pais();
+            }
+        } while (opcion != 5);
+    }
+    
+    public static void switch_editorial() {
+        int opcion;
+        do {
+            menu_secundario();
+            opcion = Leer.entero();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    switches();
+                    break;
+                default:
+                switch_editorial();
+            }
+        } while (opcion != 5);
+    }
+    
+    public static void switch_usuario() {
+        int opcion;
+        do {
+            menu_secundario();
+            opcion = Leer.entero();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    switches();
+                    break;
+                default:
+                switch_usuario();
+            }
+        } while (opcion != 5);
+    }
+    
+    public static void switch_libro() {
+        int opcion;
+        do {
+            menu_secundario();
+            opcion = Leer.entero();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    switches();
+                    break;
+                default:
+                switch_libro();
+            }
+        } while (opcion != 5);
+    }
+    
+    public static void inicio() {
+        switches();
+    }
+    
+    public static void main(String[] args) {
+        inicio();
+    }
 
 }
